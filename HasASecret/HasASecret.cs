bc5ab982f5db77a7f4baf17229820b5dc59060ae
Amanda M. Jessup <mandajessup@gmail.com>
2021-03-13 16:37:26 -0500

    Add project files.

---
+++ HasASecret/HasASecret.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+
+namespace HasASecret
+{
+    class HasASecret
+    {
+        private string secret = "xyzzy";
+        private string dogon = "Amanda M Jessup";
+
+
+
+
+
+    }
+}
---
+++ HasASecret/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace HasASecret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HasASecret keeper = new HasASecret();
+
+            FieldInfo[] fields = keeper.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach(FieldInfo field in fields)
+            {
+                Console.WriteLine(field.GetValue(keeper));
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
